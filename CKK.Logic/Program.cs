db2c7a88793d84a145b3fa31d424f0d743cc841f
Kohle Christensen <Kohle.Christensen3787@omail.otech.edu>
2022-05-19 19:45:46 -0600

    Add project files.

---
+++ CKK.Logic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CKK.Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
