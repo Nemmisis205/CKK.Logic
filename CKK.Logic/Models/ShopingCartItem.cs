b4af37d41c3d241f551910edc9f345271a299768
4400113787 <4400113787@BT144-PROG01>
2022-05-26 19:07:15 -0600

    Project Deliverables 2. Added classes as per UML.

---
+++ CKK.Logic/Models/ShopingCartItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    class ShoppingCartItem
+    {
+        private Product _product;
+        private int _quantity;
+
+        public ShoppingCartItem(Product product, int quantity)
+        {
+            _product = product;
+            _quantity = quantity;
+        }
+        public int GetQuantity() { return _quantity; }
+        public SetQuantity(int quantity) { _quantity = quantity; }
+        public Product GetProduct() { return _product; }
+        public SetProduct(Product product) { _product = product; }
+    }
+}
